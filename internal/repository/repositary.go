67129838daee4bccf162192440deb5b8b5ffda0c
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-03-18 21:19:51 +0545

    added the task method for the TaskRepo(retrieves the task from database and converts them into usable go objects

--- internal/repository/repositary.go
+++ internal/repository/repositary.go
@@ -1,7 +1,10 @@
 package repository
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
+	"taskmanager/internal/model"
 )
 
 type postgresTaskRepo struct {
@@ -18,4 +21,25 @@ func NewTaskRepo(db *sql.DB) TaskRepositary {
 
 func NewAuthRepo(db *sql.DB) AuthRepositary {
 	return &postgresAuthRepo{db: db}
+}
+
+// this part will contain the task methods //
+
+func (r *postgresTaskRepo) GetAll(ctx context.Context) ([]model.Task, error) {
+	rows, err := r.db.QueryContext(ctx, "SELECT id,title,description,status,created_at FROM tasks")
+	if err != nil {
+		return nil, fmt.Errorf("taskRepoGetAll :%w", err)
+	}
+	defer rows.Close()
+
+	var tasks []model.Task
+	for rows.Next() {
+		var t model.Task
+		if err := rows.Scan(&t.Id, &t.Title, &t.Description, &t.Status, &t.CreatedAt); err != nil {
+			return nil, fmt.Errorf("taskRepo.GetAll :%w", err)
+
+		}
+		tasks = append(tasks, t)
+	}
+	return tasks, nil
 }
